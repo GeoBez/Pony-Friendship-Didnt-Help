e0f17d50408023b05462856ad260cfc1e13685a0
GeoBez <bezdezscki@mail.ru>
2022-03-25 14:18:32 +0500

    Разделенный скрипт врагов

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,27 +5,12 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 	public float health;
-	public float speed;
-
-	private Transform _target;
-	public string target = "Player";
-
-	void Start()
-	{
-		_target = GameObject.FindGameObjectWithTag(target).GetComponent<Transform>();
-	}
 
     void Update()
     {
 		if (health <= 0)
 		{
 			Death();
-		}
-
-		if (_target != null) 
-		{
-			transform.eulerAngles = _target.transform.position.x > transform.position.x ? new Vector3(0, 180, 0) : new Vector3(0, 0, 0);
-			transform.position = Vector2.MoveTowards(transform.position, _target.transform.position, speed * Time.deltaTime);
 		}
 	}
 
