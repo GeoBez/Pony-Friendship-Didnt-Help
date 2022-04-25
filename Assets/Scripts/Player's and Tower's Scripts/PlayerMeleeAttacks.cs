9fb9ea1704be7ac4739f27a1af8c96ccf8ba108e
LexKeivi <lex.keivi@mail.ru>
2022-04-25 19:38:43 +0500

    Ближний бой...
    ну или по крайней мере его имитация)

---
+++ Assets/Scripts/Player's and Tower's Scripts/PlayerMeleeAttacks.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerMeleeAttacks : MonoBehaviour
+{
+    public float attackCoolDown;
+    public HealthBar attackCoolDownBar;
+    private bool isNeedCountdown;
+    private float currentAttackCoolDown;
+    public float attackDamage;
+    public float attackRange;
+    public LayerMask enemyLayer;
+    [SerializeField] private Animator animator;
+    
+    void Start()
+    {
+        currentAttackCoolDown = attackCoolDown;
+        attackCoolDownBar.SetMaxHealth(attackCoolDown);
+    }
+    void Update()
+    {
+        if (currentAttackCoolDown == attackCoolDown) TargetSearch();
+
+        if (isNeedCountdown) currentAttackCoolDown -= Time.deltaTime;
+
+        if (currentAttackCoolDown <= 0)
+        {
+            isNeedCountdown = false;
+            currentAttackCoolDown = attackCoolDown;
+        }
+        attackCoolDownBar.SetHealth(currentAttackCoolDown);
+    }
+
+    private void Attack(Collider2D[] hitEnemies)
+    {
+        animator.SetTrigger("Attack");
+
+        foreach(var enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>()?.TakeDamage(attackDamage);
+        }
+
+        isNeedCountdown = true;
+    }
+
+    private void TargetSearch()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayer);
+
+        if (hitEnemies.Length != 0) 
+        {
+            Attack(hitEnemies);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red - new Color(0, 0, 0, 0.9F);
+        Gizmos.DrawSphere(transform.position, attackRange);
+    }
+}
