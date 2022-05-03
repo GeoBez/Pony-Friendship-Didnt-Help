ab4cea7289b8eb37e406a397f80f72eec693bcd9
KatyaZav <party2015ru@mail.ru>
2022-05-03 10:12:11 +0500

    Чутка допилила монетки
    Был разовый баг: из последнего монстра после окончания игры выпадало бесконечное количество монеток

--- Assets/Scripts/Coin_Count_Text.cs
+++ Assets/Scripts/Coin_Count_Text.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class Coin_Count_Text : MonoBehaviour
 {
-    public static int coin_Count=0;
+    public static int coin_Count;
     private Text text;
 
     void Start()
     {
-        coin_Count = 0;
+        coin_Count = 50;
         text = GetComponent<Text>();        
     }
     void Update()
