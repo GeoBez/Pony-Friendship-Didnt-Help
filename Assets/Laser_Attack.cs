94221d8f11b22637b97676bb51980a549a39622f
GeoBez <bezdezscki@mail.ru>
2022-05-22 20:04:36 +0500

    Доработал часть механик босса

--- Assets/Laser_Attack.cs
+++ Assets/Laser_Attack.cs
@@ -6,21 +6,46 @@ public class Laser_Attack : MonoBehaviour
 {
     public int damage;
     public float time;
+    public float delay;
     float default_Time;
+    float default_Delay;
     float defaultAttackTime;
+
     Enemy enemy;
     Boss_Script boss;
+    Player Player;
+
+    SpriteRenderer sprite;
+    BoxCollider2D collider;
 
     private void Awake()
     {
         enemy = GetComponentInParent<Enemy>();
         boss = GetComponentInParent<Boss_Script>();
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         defaultAttackTime = enemy.attackTime;
+        sprite = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
+        default_Delay = delay;
+        default_Time = time;
+        sprite.enabled = false;
+        collider.enabled = false;
     }
 
     private void Update()
     {
-        time -= Time.deltaTime;
+        if(delay <= 0)
+        {
+            time -= Time.deltaTime;
+            sprite.enabled = true;
+            collider.enabled = true;
+        }
+        else if(delay > 0)
+        {
+            delay -= Time.deltaTime;
+            sprite.enabled = false;
+            collider.enabled = false;
+        }
 
         if(time <= 0)
         {
@@ -28,6 +53,7 @@ public class Laser_Attack : MonoBehaviour
             boss.isAttacking = false;
             enemy.attackTime = defaultAttackTime;
             time = default_Time;
+            delay = default_Delay;
             gameObject.SetActive(false);
         }
     }
@@ -36,7 +62,7 @@ public class Laser_Attack : MonoBehaviour
     {
         if(collision.tag == "Player")
         {
-            collision.GetComponent<Player>().TakeDamage(damage);
+            Player.TakeDamage(damage);
         }
     }
 }
