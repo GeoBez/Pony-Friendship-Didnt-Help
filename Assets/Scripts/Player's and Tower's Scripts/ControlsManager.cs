a33834028ddaa37440a16c1de3be203575326639
LexKeivi <lex.keivi@mail.ru>
2022-07-06 00:22:30 +0500

    Костыльно сменил управление на клавиши

---
+++ Assets/Scripts/Player's and Tower's Scripts/ControlsManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsManager : MonoBehaviour
+{
+    public FreezeUlt freezeUlt;
+    public Tower_UI tower_UI;
+    public 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            freezeUlt.TryUse();
+        }
+        if (Input.GetKeyDown(KeyCode.F)){
+            tower_UI.CreateTower();
+        }
+    }
+}
