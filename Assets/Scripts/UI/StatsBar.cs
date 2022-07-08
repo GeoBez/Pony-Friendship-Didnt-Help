4a4d710d71044bfc75f91e331cdc124d8ed4f4cf
LexKeivi <lex.keivi@mail.ru>
2022-07-08 23:15:59 +0500

    Обновил UI и хп

--- Assets/Scripts/UI/StatsBar.cs
+++ Assets/Scripts/UI/StatsBar.cs
@@ -8,16 +8,35 @@ public class StatsBar : MonoBehaviour
     public Slider slider;
     public Gradient gradient;
     public Image fill;
+    private Text hpCount;
+    private RectTransform _rectTransform;
+
+    void Awake(){
+        hpCount = GetComponentInChildren<Text>();
+        _rectTransform = GetComponent<RectTransform>();
+    }
+    
     public void SetMaxValue(float value)
     {
         slider.maxValue = value;
         slider.value = value;
 
         fill.color = gradient.Evaluate(1f);
+        
+        if (hpCount != null) hpCount.text = $"{slider.maxValue}/{slider.maxValue}";
     }
     public void SetValue(float value)
     {
         slider.value = value;
         fill.color = gradient.Evaluate(slider.normalizedValue);
+        if (hpCount != null) hpCount.text = $"{value}/{slider.maxValue}";
+    }
+    
+    void Update()
+    {
+        if (_rectTransform != null)
+        {
+            _rectTransform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
